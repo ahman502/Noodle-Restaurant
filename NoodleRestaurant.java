095d3516f4d92e098864d0d96dd12504a06f32bf
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2021-08-23 20:27:37 -0400

    Update NoodleRestaurant.java

--- NoodleRestaurant.java
+++ NoodleRestaurant.java
@@ -1,26 +1,32 @@
 class NoodleRestaurant {
   String name;
 
+  //noodlerestaurant constructor
   public NoodleRestaurant(String customerName){
     name = customerName;
   }
 
+  //passing noodle object as an argument 
   public void order(Noodle noodle) {
     System.out.println(noodle.getCookPrep());
     System.out.println("Order for " + name + " is ready.");
   }
 
   public static void main(String[] args) {
+    //creating new instance of Ramen noodle and Pho noodle
     Noodle ramen, pho;
     ramen = new Ramen();
     pho = new Pho();
 
+    //passing two new customers' names
     NoodleRestaurant customer1 = new NoodleRestaurant("Sagirah");
     NoodleRestaurant customer2 = new NoodleRestaurant("Maritza");
 
-    // Add your code here
-  customer1.order(pho);
-  customer2.order(ramen);
+   
+    //customer 1 has ordered pho so the getCookPrep() method for pho will be called
+    customer1.order(pho);
+    //customer 2 has ordered ramen so the getCookPrep() method for ramen will be called
+    customer2.order(ramen);
 
 
   }
