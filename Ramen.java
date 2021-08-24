5490884cc071f6974b9d45c943f02000e94e1f98
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2021-08-23 20:30:50 -0400

    Update Ramen.java

--- Ramen.java
+++ Ramen.java
@@ -1,11 +1,12 @@
 class Ramen extends Noodle {
 
   Ramen() {
-
+    //Ramen calls the constructor of the super class (Noodle, in this case) and passes it's own porpeties into the constructor
     super(30.0, 0.3, "flat", "wheat flour");
 
   }
 
+  //here we do some method overriding to give the customer directions for a specific kind of noodle (Ramen, in this case)
   @Override
   public String getCookPrep() {
 
