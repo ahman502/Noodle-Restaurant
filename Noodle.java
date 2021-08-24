6d9545b7f7f4ee4fea3045b76fe53c4adab127b1
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2021-08-23 20:22:04 -0400

    Update Noodle.java

--- Noodle.java
+++ Noodle.java
@@ -1,11 +1,13 @@
 class Noodle {
 
+  //instance fields
   protected double lengthInCentimeters;
   protected double widthInCentimeters;
   protected String shape;
   protected String ingredients;
   protected String texture = "brittle";
 
+  //Noodle contructor
   Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
 
     this.lengthInCentimeters = lenInCent;
@@ -15,6 +17,7 @@ class Noodle {
 
   }
 
+  //default method used for all kinds of noodles
   public String getCookPrep() {
 
     return "Boil noodle for 7 minutes and add sauce.";
